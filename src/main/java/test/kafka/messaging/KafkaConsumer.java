1dd35399c87af2d181b33a495cad8db575aa1289
season <sionwer4@naver.com>
2023-08-11 21:16:24 +0900

    feat: Create kafka consumer

--- src/main/java/test/kafka/messaging/KafkaConsumer.java
+++ src/main/java/test/kafka/messaging/KafkaConsumer.java
@@ -1,4 +1,16 @@
 package test.kafka.messaging;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
 public class KafkaConsumer {
+
+    @KafkaListener(topics="demo_spring")
+    public void listener(String data){
+        log.info("Received message={}", data);
+    }
+
 }
