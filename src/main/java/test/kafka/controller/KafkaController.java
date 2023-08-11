15d7b6ea2c446c960025b67864c5ddbcf34b0285
season <sionwer4@naver.com>
2023-08-11 21:19:37 +0900

    feat: Create kafka message sending controller

---
+++ src/main/java/test/kafka/controller/KafkaController.java
@@ -0,0 +1,21 @@
+package test.kafka.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import test.kafka.messaging.KafkaProducer;
+
+@RestController
+public class KafkaController {
+
+    private final KafkaProducer kafkaProducer;
+
+    public KafkaController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @GetMapping("/send")
+    public void sendMessage(@RequestBody String topic, String payload){
+        kafkaProducer.sendMessage(topic, payload);
+    }
+}
