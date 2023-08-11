96f5681b5276e0aca44b70e4757a8630058076aa
season <sionwer4@naver.com>
2023-08-11 21:34:44 +0900

    fix: Add group id into consumer configuration

--- src/main/java/test/kafka/configuration/KafkaConsumerConfig.java
+++ src/main/java/test/kafka/configuration/KafkaConsumerConfig.java
@@ -16,12 +16,16 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
-    @Value(value = "${kafka.bootstrapAddress}")
+    @Value(value = "${kafka.bootstrap-address}")
     private String bootstrapAddress;
+
+    @Value(value = "${kafka.group-id}")
+    private String groupId;
 
     public Map<String, Object> consumerConfig(){
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
